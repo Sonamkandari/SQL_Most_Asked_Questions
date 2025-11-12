029beaf0a4e8982b0327da74a415d95795b6e1f3
Sonam Kandari <145662567+Sonamkandari@users.noreply.github.com>
2025-11-13 01:22:32 +0530

    Update most_asked_ques.sql

--- most_asked_ques.sql
+++ most_asked_ques.sql
@@ -1,1 +1,9 @@
+-- second highest salary
+select max(salary) from Employee 
+where salary<(select max(salary)from employee);
 
+-- Another way of doing the same question
+select (select distinct salary from employee 
+order by salary desc limit 1 offset 1) as SecondHigestSalary;
+
+-- 
