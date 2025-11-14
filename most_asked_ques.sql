fccdf9189572723282e72bcb086b7585fb5ff244
Sonam Kandari <145662567+Sonamkandari@users.noreply.github.com>
2025-11-14 12:07:21 +0530

    Update most_asked_ques.sql

--- most_asked_ques.sql
+++ most_asked_ques.sql
@@ -35,3 +35,11 @@ where id%!=0 And description!='boring' order
 by rating desc;
 
 -- Find Customer Referee
+
+-- Customer Placing the largest Number of Orders 
+select customer_number from Orders
+group by customer_number
+order by count(distinct order_number) desc limit 1
+
+
+
